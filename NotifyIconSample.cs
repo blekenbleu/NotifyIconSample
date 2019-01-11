9bf26aed1a6efcbac74cf869620eebcfd52f9958
Steven Carmichael <blekenbleu@gmail.com>
2019-01-11 18:02:36 -0500

    Useful demo
    Added observations to README
    Added more interaction to the C# source

--- NotifyIconSample.cs
+++ NotifyIconSample.cs
@@ -1,9 +1,12 @@
 /* https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.notifyicon?view=netframework-4.7.2
-   This code example uses the NotifyIcon class to display an icon for an application in the notification area.
-   It demonstrates setting the Icon, ContextMenu, Text, and Visible properties and handling the DoubleClick event.
-   A ContextMenu with an Exit item on it is assigned to the NotifyIcon.ContextMenu property,
-    which allows the user to close the application.
-   When the DoubleClick event occurs, the application form is activated by calling the Form.Activate method.
+   This code example displays an application icon in the notification area usiing NotifyIcon.
+   It demonstrates Icon, ContextMenu, Text, Visible properties and DoubleClick event handling.
+   A ContextMenu Close item is assigned to the NotifyIcon.ContextMenu property,
+    allowing a user to close the application.
+   Another ContextMenu event handler invokes another Class' method,
+     which in turn uses a main class method to change an Icon Balloon string.
+   A DoubleClick event hides or activates the application form using Form.Activate method.  
+   A Form size change event conditionally activates an Icon Balloon.  
  */
 using System;
 using System.Drawing;
@@ -11,76 +14,141 @@ using System.Windows.Forms;
 
 public class Form1 : System.Windows.Forms.Form
 {
+    public static Form1 form1; // = new Form1(); // Place this var out of the constructor
     private System.Windows.Forms.NotifyIcon notifyIcon1;
-    private readonly System.Windows.Forms.ContextMenu contextMenu1;
+    private readonly System.Windows.Forms.ContextMenu RightClickMenu;
     private readonly System.Windows.Forms.MenuItem menuItem1;
-    private System.ComponentModel.IContainer components;
-    
+    private System.ComponentModel.IContainer container;
+    // track state change to set notifyIcon1 Balloon
+    private FormWindowState PreviousWindowState;
+    // access a SubClass method
+    private readonly MySubclass subclass;
+
     [STAThread]
     static void Main() 
     {
-        Application.Run(new Form1());
+        Application.Run(form1 = new Form1());
     }
 
     public Form1()
     {
-        this.components = new System.ComponentModel.Container();
-        this.contextMenu1 = new System.Windows.Forms.ContextMenu();
-        this.menuItem1 = new System.Windows.Forms.MenuItem();
+        // Provide a hook for disposing notifyIcon1
+        this.container = new System.ComponentModel.Container();
+        this.RightClickMenu = new System.Windows.Forms.ContextMenu();
+        this.menuItem1 = new System.Windows.Forms.MenuItem("E&xit");
+        this.subclass = new MySubclass();
 
-        // Initialize contextMenu1
-        this.contextMenu1.MenuItems.AddRange(
-                    new System.Windows.Forms.MenuItem[] {this.menuItem1});
+        // Initialize RightClickMenu
+        this.RightClickMenu.MenuItems.Add(this.menuItem1);
+        this.RightClickMenu.MenuItems.Add("Subclass?", MySubclass);
 
         // Initialize menuItem1
         this.menuItem1.Index = 0;
-        this.menuItem1.Text = "E&xit";
-        this.menuItem1.Click += new System.EventHandler(this.MenuItem1_Click);
+        this.menuItem1.Click += new System.EventHandler(this.MenuItem1_Close);
 
         // Create the NotifyIcon.
-        notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+        notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.container);
 
         notifyIcon1.Visible = true;
 
         // Set the icon that will appear in the systray.
         notifyIcon1.Icon = new Icon("appicon.ico");
 
-        // Sets the menu appears when the systray icon is right clicked.
-        notifyIcon1.ContextMenu = this.contextMenu1;
+        // Set systray icon right-click menu
+        notifyIcon1.ContextMenu = this.RightClickMenu;
 
-        // Set the text that displays in a tooltip
-        // when the mouse hovers over the systray icon.
+        // Set text for tooltip to display
+        // when hovering mouse over the systray icon.
         notifyIcon1.Text = "Form1 NotifyIcon Icon";
         notifyIcon1.BalloonTipTitle = "Form1 notifyIcon1 Balloon Tip Title";
 
-        // Handle the DoubleClick event to activate the form.
+        // DoubleClick event handler activates the form.
         notifyIcon1.DoubleClick += new System.EventHandler(this.NotifyIcon1_DoubleClick);
 
-        // Pay attention to Form1 minimize
+        // control FormState() MessageBox
+        boxing = true;
+
+        // Form1 minimize events launch BallonTips
         this.Resize += new System.EventHandler(this.Form1_Resize);
 
-        /* You get a form, whether or not it is wanted.
+        /* You get a form, whether wanted or not.
          * It insists on being initially visible
-         * unless minimized and hidden there.
+         * unless minimized and hidden on the taskbar.
          */
-        this.WindowState = FormWindowState.Minimized;
+        FormState("Before Minimixe");
+        PreviousWindowState = this.WindowState = FormWindowState.Minimized;
         this.ShowInTaskbar = false;
-        this.Visible = false;
+
+        // track Window size changes
+        resize_count = 0;
+
+/* Redundant and pointless, here:
+        this.Hide();
+ */
+
+// Visible becomes true AFTER leaving - where documented?
+//      FormState("Leaving Form1");
     }
 
     protected override void Dispose( bool disposing )
     {
-        // Clean up any components being used.
+        // Clean up any container being used.
         if( disposing )
-            if (components != null)
-                components.Dispose();            
+            if (container != null)
+                container.Dispose();            
 
         base.Dispose( disposing );
     }
 
+    private bool boxing;
+    private System.Windows.Forms.DialogResult button;
+    internal void FormState(string current)
+    {
+        if (boxing)
+        {
+            button = System.Windows.Forms.MessageBox.Show(
+                String.Format("WindowState {0}, Visible {1}, ShowInTaskbar {2}\nPress Cancel to disable these prompts",
+                this.WindowState, this.Visible, this.ShowInTaskbar),
+                current,
+                System.Windows.Forms.MessageBoxButtons.OKCancel,
+                System.Windows.Forms.MessageBoxIcon.Information);
+            boxing = (button == System.Windows.Forms.DialogResult.OK);
+        }
+    }
+
+    private int resize_count;
+    internal void MsgBoxResize()
+    {
+        resize_count++;
+
+        FormState(String.Format("Resize Count {0}", resize_count));
+    }
+
+    // Use Resize events to check state
+    private void Form1_Resize(object sender, EventArgs e)
+    {
+        MsgBoxResize();
+        // New plan
+        if (PreviousWindowState != WindowState)
+            if (WindowState == FormWindowState.Minimized)
+            {
+                if (0 == notifyIcon1.BalloonTipText.Length || notifyIcon1.BalloonTipText == this.subclass.text)
+                    notifyIcon1.BalloonTipText = "Form Minimize";
+                notifyIcon1.ShowBalloonTip(5000);
+            }
+            else
+            {
+                notifyIcon1.Visible = false;
+                notifyIcon1.Visible = true; // trick to kill BalloonTip when Form1 is no longer minimized;
+            }
+
+        PreviousWindowState = WindowState;
+    }
+
     private void NotifyIcon1_DoubleClick(object Sender, EventArgs e) 
     {
-        // Show the form when the user double clicks on the notify icon.
+        // Show the form when users double clicks notify icon.
+        FormState("Entering NotifyIcon1");
         if (0 == this.Text.Length)
 	    {
             // Set up how the form should be displayed.
@@ -91,9 +159,10 @@ public class Form1 : System.Windows.Forms.Form
             this.Visible = true;
         }
 
-        // Set the WindowState to normal if the form is minimized.
+        // Set WindowState to normal if Form is minimized.
         if (this.WindowState == FormWindowState.Minimized)
             this.WindowState = FormWindowState.Normal;
+        // else toggle Form visibility
         else if (this.Visible)
             this.Hide();
         else this.Show();
@@ -103,21 +172,36 @@ public class Form1 : System.Windows.Forms.Form
             this.Activate();
     }
 
-    private void MenuItem1_Click(object Sender, EventArgs e) {
+    private void MySubclass(object Sender, EventArgs e) {
+        // Invoke a subclass method
+        this.subclass.SubBall();
+    }
+
+    private void MenuItem1_Close(object Sender, EventArgs e) {
         // Close the form, which closes the application.
         this.Close();
     }
 
-// Use the Resize event to check the Forms.WindowState property.
-    private void Form1_Resize(object sender, EventArgs e)
+    internal void Balloon(string text)
     {
-        if (WindowState == FormWindowState.Minimized && this.Visible)
-        {
-            // First event is invalid
-            if (0 == notifyIcon1.BalloonTipText.Length)
-                notifyIcon1.BalloonTipText = "Form minimized.";
-            else
-              notifyIcon1.ShowBalloonTip(5000);
-        }
+        notifyIcon1.BalloonTipText = text;
+        notifyIcon1.ShowBalloonTip(5000);
+    }
+
+}   // Class Form1
+
+// practice methods among classes
+public class MySubclass
+{
+    internal readonly string text;
+
+    public MySubclass()
+    {
+        text = "Subclass";
+    }
+
+    internal void SubBall()
+    {
+        Form1.form1.Balloon(text);
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # NotifyIconSample
-Minimal example of NotifyIcon app with initially invisible form.  Adapted from .NET [NotifyIcon Class](https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.notifyicon?view=netframework-4.7.2) Example
+Minimalist example of NotifyIcon app with initially invisible form.  Adapted from .NET [NotifyIcon Class](https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.notifyicon?view=netframework-4.7.2) Example
 ## Background
 Years ago, I wrote ANSI C Win32 utilities using [Rector & Newcomer "Win32 Programming"](https://www.amazon.com/dp/B006PU8B8C).  
 So much as possible since then, C++ and objective C  were avoided *because ugly*.  
@@ -10,4 +10,10 @@ The eventual goal is filter programs that mostly sit quietly on the notification
 but pop up balloons, dialog boxes and/or frames as appropriate.  
 After trying to hack other GitHub repositories to suit,  
 first sorting the proposed user interface seems a better approach.  
-Meanwhile, Albahari's ["C# 7.0 Pocket Reference"](https://www.amazon.com/gp/product/1491988533) is on the way..
+Meanwhile, Albahari's ["C# 7.0 Pocket Reference"](https://www.amazon.com/gp/product/1491988533) is on the way..  
+* Initial Form State is Normal, Visible == False, ShowInTaskbar == True 
+* 8 Form Resize events before first user-initiated minimize
+* Application form defaults to hidden in the constructor,
+  but becomes immediately visible after unless minimized and hidden on the taskbar.
+* Calling class main methods from other classes requires a Application.Run() with a public static instance.
+* Calling other class methods from main wants a readonly instance of that class.
